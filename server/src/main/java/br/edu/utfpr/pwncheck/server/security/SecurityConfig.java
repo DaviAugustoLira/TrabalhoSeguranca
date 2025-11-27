cb5b74867600d5055125f5433043854ad4469cff
Davi <daviaugustolira@gmail.com>
2025-11-27 20:32:10 -0300

    Arrumando CORS

--- server/src/main/java/br/edu/utfpr/pwncheck/server/security/SecurityConfig.java
+++ server/src/main/java/br/edu/utfpr/pwncheck/server/security/SecurityConfig.java
@@ -6,6 +6,11 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -20,10 +25,31 @@ public class SecurityConfig {
                 )
                 .httpBasic(Customizer.withDefaults());           // opcional: pode remover se não quiser basic auth
 
+        http.cors(cors -> corsConfigurationSource());
+
         // Se quiser desabilitar completamente httpBasic e formLogin:
         // .httpBasic(AbstractHttpConfigurer::disable)
         // .formLogin(AbstractHttpConfigurer::disable)
 
         return http.build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        // Lista das origens autorizadas, no nosso caso que iremos rodar a aplicação localmente o * poderia ser trocado
+        // por: http://localhost:porta, em que :porta será a porta em que a aplicação cliente será executada
+        configuration.setAllowedOrigins(List.of("*"));
+        // Lista dos métodos HTTP autorizados
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT"));
+        // Lista dos Headers autorizados, o Authorization será o header que iremos utilizar para transferir o Token
+        configuration.setAllowedHeaders(List.of("Authorization","x-xsrf-token",
+                "Access-Control-Allow-Headers", "Origin",
+                "Accept", "X-Requested-With", "Content-Type",
+                "Access-Control-Request-Method",
+                "Access-Control-Request-Headers", "Auth-Id-Token"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
